82c4f6ca2d62486e89036297b79d5f4abbec4073
Jed Lau <jedlau@gmail.com>
2014-11-25 17:58:19 -0800

    swifting it up

--- cartograph2/cartograph2/AppDelegate.swift
+++ cartograph2/cartograph2/AppDelegate.swift
@@ -6,16 +6,29 @@
 //  Copyright (c) 2014 Jed Lau. All rights reserved.
 //
 
+import Photos
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var previousFetchResult: PHFetchResult?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.NotDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status: PHAuthorizationStatus) -> Void in
+            })
+        }
+
+        // Ask to send local notifications.
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: nil))
+
+        // Set the minimum background fetch interval.
+        application.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
+        
         return true
     }
 
@@ -40,7 +53,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.Authorized {
+            var currentFetchResult = PHCollectionList.fetchMomentListsWithSubtype(PHCollectionListSubtype.MomentListCluster, options: nil);
+            
+            if self.previousFetchResult != nil {
+                // Compare against previous fetch results.
+                var fetchResultChangeDetails = PHFetchResultChangeDetails(fromFetchResult:self.previousFetchResult, toFetchResult:currentFetchResult, changedObjects:nil)
+                
+                self.previousFetchResult = currentFetchResult
+                
+                if fetchResultChangeDetails.hasIncrementalChanges {
+                    // Get new moments.
+                    
+                    // Call backend to determine if any moments may be shared.
+                    
+                    presentLocalNotification()
+                    completionHandler(UIBackgroundFetchResult.NewData)
+                    return
+                }
+            } else {
+                // No previous fetch result.
+                if currentFetchResult.count > 0 {
+                    presentLocalNotification()
+                    completionHandler(UIBackgroundFetchResult.NewData)
+                    return
+                }
+            }
+        }
 
+        completionHandler(UIBackgroundFetchResult.NoData)
+    }
+    
+    func presentLocalNotification() {
+        dispatch_async(dispatch_get_main_queue(), { [weak self] () -> Void in
+            // Display a location notification.
+            var localNotification:UILocalNotification = UILocalNotification()
+            localNotification.alertBody = "Did you see nicoco, hechanova, or 3 other friends yesterday?"
+            localNotification.alertAction = "view detected moments"
+            UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        })
+    }
 
 }
 
--- cartograph2/cartograph2/ViewController.swift
+++ cartograph2/cartograph2/ViewController.swift
@@ -6,56 +6,20 @@
 //  Copyright (c) 2014 Jed Lau. All rights reserved.
 //
 
-import UIKit
 import Photos
+import UIKit
 
-class ViewController: UIViewController, PHPhotoLibraryChangeObserver {
+class ViewController: UIViewController {
     let photoLibrary = PHPhotoLibrary.sharedPhotoLibrary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        switch PHPhotoLibrary.authorizationStatus() {
-        case PHAuthorizationStatus.NotDetermined:
-            PHPhotoLibrary.requestAuthorization({ (status: PHAuthorizationStatus) -> Void in
-                if status == PHAuthorizationStatus.Authorized {
-                    self.photoLibrary.registerChangeObserver(self)
-                    self.fetchAssets()
-                }
-            })
-
-        case PHAuthorizationStatus.Authorized:
-            self.photoLibrary.registerChangeObserver(self)
-            self.fetchAssets()
-            
-        default:
-            // Show an alert.
-            break
-        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: -
-    
-    func fetchAssets() {
-        let fetchResult = PHAsset.fetchAssetsWithOptions(nil)
-        if fetchResult.firstObject == nil {
-            NSLog("Error: asset not fetched")
-            return
-        }
-        let asset = fetchResult.firstObject! as PHAsset
-    }
-    
-    // MARK: - PHPhotoLibraryChangeObserver
-    
-    func photoLibraryDidChange(changeInfo: PHChange!) {
-        dispatch_async(dispatch_get_main_queue(), { [weak self] () -> Void in
-            NSLog("PhotoLibrary: Asset changed")
-        })
     }
 }
 
